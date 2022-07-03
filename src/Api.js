44310ff8c46dd1b40e46319cdd59630c27636bab
Atharva <scifacts001@gmail.com>
2022-07-03 18:48:41 +0530

    Added text to voice

--- src/Api.js
+++ src/Api.js
@@ -1,31 +1,48 @@
 import { useState} from 'react'
 import axios from 'axios'
+import {useSpeechSynthesis} from 'react-speech-kit';
 
 function Api() {
 
     const [catfact, setcatFact] = useState('');
+    const {speak} = useSpeechSynthesis();
 
-    const Api = () => {
-        
+  
+
+    const Api1 = () => {
+      
         return axios.get("https://catfact.ninja/fact")
               .then((response) => {
                console.log(response.data.fact)
                setcatFact(response.data.fact)
-            })
               
+            })
+            
             }
+
+            const SpeakFunc = () =>{
+              {speak({text: catfact})}
+            }
+
+           
 
   return (
     <div><h1>Cat Facts Api</h1>
     <br/>
         
     <div className='center'>
-    <button className='button' onClick={Api }>Click</button>
+    <button className='button' onClick={Api1}>Get Facts</button>
+    <button className='button' onClick={SpeakFunc}>Listen it</button>
+    
     </div>
     
+    
     <div className='card'>
-        <h3>{catfact}</h3>
+        <h3>{catfact} </h3>
+        
         </div>
+       
+        
     </div>
   )
 }
